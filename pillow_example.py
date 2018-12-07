6a788443bc0d005472a02b0e9f2082c10a8ea6f0
Timofey Khirianov <tkhirianov@gmail.com>
2018-12-07 14:51:12 +0300

    Исправил баги в отображении картинки

--- pillow_example.py
+++ pillow_example.py
@@ -11,13 +11,16 @@ class MainWindow:
         self.canvas = tkinter.Canvas(self.root)
         self.canvas.pack()
         
+        self.ball_image = ImageTk.PhotoImage(Image.open('magic_ball.png'))
+
+        
         self.balls = []
         
 
     def _create_menu(self):
         menu = tkinter.Menu(self.root)
         file_menu = tkinter.Menu(menu)
-        file_menu.add_command(label="Exit", command=hello)
+        file_menu.add_command(label="Exit", command=self._exit)
         file_menu.add_command(label="Start game",
                               command=self._start_game)
         menu.add_cascade(label="File", menu=file_menu)
@@ -29,7 +32,7 @@ class MainWindow:
         
     def _start_game(self):
         print("Started game.")
-        self.balls.append(Ball(self.canvas, 100, 100))
+        self.balls.append(Ball(self.canvas, self.ball_image, 100, 100))
         
     def loopback(self):
         print("Hello!")
@@ -40,14 +43,11 @@ class MainWindow:
         self.root.mainloop()
 
 
-class Ball:
-    image_file = 'magic_ball.png'
-    ball_image = ImageTk.PhotoImage(Image.open(self.image_file))
-    
-    def __init__(self, canvas, x, y):
+class Ball:   
+    def __init__(self, canvas, ball_image, x, y):
         self.x = x
         self.y = y
-        self.avatar = canvas.create_image(x, y, image=self.ball_image)
+        self.avatar = canvas.create_image(x, y, image=ball_image)
 
 
 def main():
