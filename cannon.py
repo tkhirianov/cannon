e5b0ca7868cb42f7fff9f245f41690aebb09dd55
Timofey Khirianov <tkhirianov@gmail.com>
2020-10-28 11:39:50 +0300

    last additions'

--- cannon.py
+++ cannon.py
@@ -1,6 +1,13 @@
+import random as rnd
 import math
+import pygame
 
-g = 9.8  # Ускорение свободного падения для снаряда.
+from my_colors import *
+
+FPS = 20
+GRAVITY_ACCELERATION = 9.8  # Ускорение свободного падения для снаряда.
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+
 
 class Cannon:
     max_velocity = 10
@@ -31,15 +38,20 @@ class Cannon:
         """
         pass
 
+    def draw(self):
+        pygame.draw.circle(screen, self.color,
+                           (int(round(self.x)), int(round(self.y))), self.r)
+
+
 class Shell:
-    standard_radius = 5
+    standard_radius = 25
 
     def __init__(self, x, y, Vx, Vy):
         self.x, self.y = x, y
         self.Vx, self.Vy = Vx, Vy
-        self.r = standard_raduis
+        self.r = Shell.standard_radius
 
-    def go(self, dt):
+    def move(self, dt):
         """
         Сдвигает снаряд исходя из его кинематических характеристик
         и длины кванта времени dt
@@ -47,12 +59,16 @@ class Shell:
         :param dt:
         :return:
         """
-        ax, ay = 0, g
+        ax, ay = 0, GRAVITY_ACCELERATION
         self.x += self.Vx*dt + ax*(dt**2)/2
         self.y += self.Vy*dt + ay*(dt**2)/2
         self.Vx += ax*dt
         self.Vy += ay*dt
         # TODO: Уничтожать (в статус deleted) снаряд, когда он касается земли.
+
+    def draw(self):
+        pygame.draw.circle(screen, self.color,
+                           (int(round(self.x)), int(round(self.y))), self.r)
 
     def detect_collision(self, other):
         """
@@ -65,27 +81,32 @@ class Shell:
 
 
 class Target:
-    standard_radius = 5
+    standard_radius = 15
 
     def __init__(self, x, y, Vx, Vy):
         self.x, self.y = x, y
         self.Vx, self.Vy = Vx, Vy
-        self.r = standard_raduis
+        self.r = Target.standard_radius
+        self.color = COLORS[rnd.randint(0, len(COLORS) - 1)]
 
-    def go(self, dt):
+    def move(self, dt):
         """
         Сдвигает шарик-мишень исходя из его кинематических характеристик
         и длины кванта времени dt
         в новое положение, а также меняет его скорость.
         :param dt:
-        :return:
+            :return:
         """
-        ax, ay = 0, g
+        ax, ay = 0, GRAVITY_ACCELERATION
         self.x += self.Vx * dt
         self.y += self.Vy * dt
         self.Vx += ax * dt
         self.Vy += ay * dt
         # TODO: Шарики-мишени должны отражаться от стенок
+
+    def draw(self):
+        pygame.draw.circle(screen, self.color,
+                           (int(round(self.x)), int(round(self.y))), self.r)
 
     def collide(self, other):
         """
@@ -96,17 +117,53 @@ class Target:
         pass  #TODO
 
 
+class Bomb:
+    pass
 
 
+def generate_random_targets(number: int):
+    targets = []
+    for i in range(number):
+        x = rnd.randint(0, SCREEN_HEIGHT)
+        y = rnd.randint(0, SCREEN_HEIGHT)
+        Vx = rnd.randint(-30, +30)
+        Vy = rnd.randint(-30, +30)
+        target = Target(x, y, Vx, Vy)
+        targets.append(target)
+    return targets
 
 
+def game_main_loop():
+
+    targets = generate_random_targets(10)
+
+    clock = pygame.time.Clock()
+    finished = False
+
+    while not finished:
+        dt = clock.tick(FPS) / 1000
+        print(dt)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print('Click!')
+
+        pygame.display.update()
+        screen.fill(GRAY)
+
+        for target in targets:
+            target.move(dt)
+
+        for target in targets:
+            target.draw()
+
+    pygame.quit()
 
 
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.update()
 
-
-
-
-
-
-
-
+    game_main_loop()
